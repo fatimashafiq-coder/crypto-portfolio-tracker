da05d887fbc18d9b6d8d283b7b38f9ec7a4b633a
workfatima829 <workfatima829@gmail.com>
2025-11-05 20:25:13 +0500

    Added routing for pages and created Header page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
 import './App.css'
 import InvestmentFields from './investmentFields/InvestmentFields'
+import Dashboard from './pages/Dashboard'
+import Investments from './pages/Investments'
+import Investment from './pages/Investment'
+import EditInvestment from './pages/EditInvestment'
+import CreateInvestment from './pages/CreateInvestment'
+import Header from './components/Header'
 
 function App() {
   return (
-    <>
-      <InvestmentFields/>
-    </>
+    <Router>
+    <Header/>
+ <InvestmentFields/>
+  <Routes>
+    <Route path='/' element={<Dashboard/>}/>
+    <Route path='/investments' element={<Investments/>}/>
+    <Route path='/investment' element={<Investment/>}/>
+    <Route path='editinvestment' element={<EditInvestment/>}/>
+    <Route path='/createinvestment' element={<CreateInvestment/>}/>
+  </Routes>
+    </Router>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+const Header = () => {
+  const data = {
+    icon: "",
+    title: "Crypto Portfolio Tracker",
+    subtitle: "Track your cryptocurrency investments with live Binance prices"
+  };
+  return (
+    <>
+          <nav className="navbar">
+        <Link to="/">Dashboard</Link>
+        <Link to="/investments">Investments</Link> 
+      </nav>
+    <header className="header">
+      <div className="header-content">
+        <span className="icon">{data.icon}</span>
+        <h1 className="title">{data.title}</h1>
+      </div>
+      <p className="subtitle">{data.subtitle}</p>
+    </header>
+    </>
+  );
+};
+export default Header;
