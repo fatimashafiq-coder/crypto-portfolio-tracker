3c66df97376373e90868289895de1feaaab3a8bb
workfatima829 <workfatima829@gmail.com>
2025-11-11 22:02:08 +0500

    Add the .env in gitignore

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
         <Link className="text-purple-700 text-xl font-semibold pr-12 no-underline" to="/investments">Investments</Link>
       </nav>
 
-      <header className="bg-gradient-to-r from-blue-500 to-purple-700 text-white text-center p-9 rounded-2xl shadow-lg font-poppins w-full">
+      <header className="bg-linear-to-r from-blue-500 to-purple-700 text-white text-center p-9 rounded-2xl shadow-lg font-poppins w-full">
         <div className="flex justify-center items-center gap-3 mb-2">
           <span className="text-3xl">{data.icon}</span>
           <h1 className="text-2xl font-bold m-0">{data.title}</h1>
